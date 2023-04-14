b3372dc13e23813e1e9f0c488a11de974346a465
jrallen724 <jrallen724@gmail.com>
2023-04-13 21:29:35 -0400

    added html, css, and script files

---
+++ script.js
@@ -0,0 +1,58 @@
+const numbers = document.querySelectorAll('.number');
+const decimal = document.querySelector('#decimal');
+const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const multiply = document.querySelector('#mutiply');
+const divide = document.querySelector('#divide');
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#equals');
+const del = document.querySelector('#delete');
+let display = document.querySelector('.display');
+let activeDisplay = true; 
+
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+
+// function add() {
+//     result = num1 + num2;
+//     console.log();
+// }
+
+// function subtract() {
+//     result = num1 - num2;
+// }
+
+// function multiply() {
+//     result = num1 * num2;
+// }
+
+// function divide() {
+//     result = num1 / num2;
+// }
+
+numbers.forEach((num) => {
+    num.addEventListener('click', () => {
+        if (display.textContent == '0') {
+            display.textContent = num.textContent;
+        } else {
+            display.textContent = display.textContent + num.textContent;
+        }
+    })
+})
+
+decimal.addEventListener('click', () => {
+    if(!activeDisplay || display.textContent.includes('.')) {
+        return;
+    } else {
+        display.textContent = display.textContent + '.';
+    }    
+})
+
+del.addEventListener('click', () => {
+    if (activeDisplay && display.textContent !== '0') {
+        display.textContent = display.textContent.slice(0, -1);
+    } 
+})
+
+ 
