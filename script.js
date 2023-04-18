5089157b0af1caaae5f69a4b5eae6734d5b46d8f
jrallen724 <jrallen724@gmail.com>
2023-04-18 14:46:51 -0400

    minor updates

--- script.js
+++ script.js
@@ -10,18 +10,30 @@ const del = document.querySelector('#delete');
 let display = document.querySelector('.display');
 let activeDisplay = true; 
 
-let num1 = 0;
+let num1;
 let num2 = 0;
 let result = 0;
 let operator = '';
+
+console.log(num1);
 
 function storeNumber(num) {
     num1 = display.textContent;
 }
 
+function operate(value1, value2, operator) {
+    switch(operator) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+    }
+    display.textContent = num1;
+}
+
 numbers.forEach((num) => {
     num.addEventListener('click', () => {
-        if (display.textContent == '0') {
+        if (display.textContent == '0' && activeDisplay) {
             display.textContent = num.textContent;
         } else {
             display.textContent = display.textContent + num.textContent;
@@ -46,14 +58,20 @@ del.addEventListener('click', () => {
     }
 })
 
- clear.addEventListener('click', () => {
+clear.addEventListener('click', () => {
     num1 = 0;
     num2 = 0;
     display.textContent = '0';
     activeDisplay = true;
- })
+})
 
- add.addEventListener('click', () => {
-    num1 = parseFloat(display.textContent);
+add.addEventListener('click', () => {
+    num2 = parseFloat(display.textContent);
     operator = add.textContent;
- })
+    console.log(num1, operator, num2);  
+    num1 = operate(num1, num2, operator);
+    console.log(num1, num2);
+    display.textContent = num1;
+    activeDisplay = false;
+})
+
